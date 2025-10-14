9c333961f7526715ade40d14e4328602bab750f7
Florent Saunier <florent.saunier13@gmail.com>
2025-10-14 11:25:28 +0200

    construction carte auvergne rhone Alpes

--- main.js
+++ main.js
@@ -4,8 +4,47 @@
     //height = 600 - margin.top - margin.bottom;
 
 
+// ================= PAGE 1 =================== //
+// Dimensions communes à la page 1
+
+const width_page1 = 600;
+const height_page1 = 500;
+
+const svg_page1 = d3.select('#graph-page1')
+    .append("svg")
+    .attr("widtg",width_page1)
+    .attr('height',height_page1)
+    .style("background", "#f9f9f9")
+    
+    
+d3.json("dataset/region-auvergne-rhone-alpes.geojson").then(function(data) {
+    console.log("Données GeoJSON chargé",data)
+});
+    // Création de la carte en pixel
+    const projection = d3.geoMercator()
+        .fitSize([width_page1,height_page1],data);
+
+    // Création des chemins pour dessiner la carte
+    const path = d3.geoPath().projection(projection);
+    
+    // Échelle couleur
+    const colorScale = d3.scaleSequential()
+        .domain([7000, 0]) // plus c’est haut, plus c’est rouge
+        .interpolator(d3.interpolateRdYlGn)
+        .unknown("#ccc");
+    
+    // Dessiner les départements
+    svg_page1.selectAll("path")
+        .data(data.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .attr("fill", "#cce5df")
+        .attr("stroke", "#333")
+        .attr("stroke-width", 0.5);
 
 
+// ================= PAGE 2 =================== //
 // Dimensions communes à la page 2
 const width_page2 = 300;
 const height_page2 = 300;
@@ -27,17 +66,18 @@ const svg_page2_graph2 = d3.select("#graph-page2-graph2")
     .append("g")
     .attr("transform", `translate(${margin_page2.left},${margin_page2.top})`);
 // lecture du fichier des départements de la région Auvergne Rhone Alpes
-const db = d3.csv("db_Auvergne_Rhone_Alpes.csv").then(function(data) {
-    data.forEach(function(d){
+d3.csv("db_Auvergne_Rhone_Alpes.csv").then(function(donnees) {
+    donnees.forEach(function(d){
         d.annee = +Number(d.annee);
         d.Code_departement = +Number(d.Code_departement);
         d.nombre = +Number(d.nombre)
 
         
     });
-    console.log(data)});
+    console.log(donnees)});
 
-// à partir d'ici il a lu le fichier db =====> suite code
+
+
 
 
 
