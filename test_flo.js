9288814616c0c03f0e5f0154a479e04c17e53a90
Florent Saunier <florent.saunier13@gmail.com>
2025-10-16 17:19:05 +0200

    last fixes

--- test_flo.js
+++ test_flo.js
@@ -7,8 +7,8 @@
 // ================= PAGE 1 =================== //
 // ====== Initialisation des constantes ==== //
 
-const width_page1 = 960;
-const height_page1 = 600;
+const width_page1 = 950;
+const height_page1 = 700;
 
 const svg_page1 = d3.select('svg')
     .attr('width',width_page1)
@@ -17,8 +17,10 @@ const svg_page1 = d3.select('svg')
 // Projection centrée Rhône-Alpes
 const projection = d3.geoMercator()
   .center([4.5, 45.75]) // centré Rhône-Alpes
-  .scale(3000)          // zoom
+  .scale(8000)          // zoom
   .translate([width_page1 / 2, height_page1 / 2]);
+
+const path = d3.geoPath().projection(projection);
 
   // Slider DOM
 const yearSlider = d3.select("#yearSlider");
@@ -32,9 +34,21 @@ Papa.parse("db_Auvergne_Rhone_Alpes.csv", {
     download : true,
     delimiter :",",
     complete : function(results) {
-        console.log("CSV chargé bite :",results.data)
+        console.log("CSV chargé v2 :",results.data)
         const data = results.data;
-        //d3.json("dataset/departements-auvergne-rhone-alpes.geojson")
+
+        d3.json("dataset/departements-auvergne-rhone-alpes.geojson").then(geojson => {
+            const paths = svg_page1.selectAll("path")
+                .data(geojson.features);
+
+            paths.enter()
+                .append("path")
+                .merge(paths)
+                .attr("d", path)
+                .attr("stroke", "#fff")
+                .attr("stroke-width", 1)
+
+        })
 
 
 
